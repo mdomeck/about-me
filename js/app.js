2cd50d3b1316ca86a95a6a17dd0f26e364bde181
mdomeck <mdomeck@aol.com>
2020-06-02 18:49:14 -0400

    JS code prompts

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
 'use strict';
 
 var firstName = 'first name';
-var meghanTeacher = 'teacher';
 
 
-var firstNameAnswer = prompt('What is your' + firstName + '?');
+var firstNameAnswer = prompt('What is your ' + firstName + '?');
 console.log('firstNameAnswer', firstNameAnswer);
 
-alert('Hello' + firstNameAnswer + ' , welcome to all about me!');
+alert('Hello ' + firstNameAnswer + ', welcome to all about me!');
 
-var meghanTeacherGuess = confirm('Was Meghan a ' + meghanTeacher + '?');
+var meghanTeacherGuess = prompt('Was Meghan a teacher?');
 console.log('meghanTeacherGuess', meghanTeacherGuess);
 
-if (meghanTeacherGuess === true){
+if (meghanTeacherGuess === 'yes' || 'Yes') {
   alert('Correct');
 } else {
   alert('Incorrect');
 }
 
-// var stateLiveResponse = 
+var stateLiveAnswer = prompt('Does Meghan live in Washington?');
+console.log('stateLiveAnswer', stateLiveAnswer);
+
+if (stateLiveAnswer === 'no' || 'No') {
+  alert('Correct, she lives in Ohio!');
+} else {
+  alert('Incorrect, she lived in Ohio!');
+}
+
+var playCelloAnswer = prompt('Does Meghan play the cello?');
+console.log('playCelloAnswer', palyCelloAnswer);
+
+if (playCelloAnswer === 'yes' || 'Yes') {
+  alert('Correct');
+} else {
+  alert('Incorrect');
+}
+
+
 
