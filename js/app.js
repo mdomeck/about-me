5a8f6832c9ba8f597900d4f2d561e6d5be21a1f5
mdomeck <mdomeck@aol.com>
2020-06-03 18:16:23 -0400

    update HTML UL and OL

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
-'use strict';
+// 'use strict';
 
-/*Ask name through prompt and give response*/
+// /*Ask name through prompt and give response*/
 
-var firstName = 'first name';
+// var firstName = 'first name';
 
-var firstNameAnswer = prompt('What is your ' + firstName + '?');
-// console.log('firstNameAnswer', firstNameAnswer);
+// var firstNameAnswer = prompt('What is your ' + firstName + '?');
+// // console.log('firstNameAnswer', firstNameAnswer);
 
-alert('Hello ' + firstNameAnswer + ', welcome to all about me!');
+// alert('Hello ' + firstNameAnswer + ', welcome to all about me!');
 
-// Prompt 5 yes or no questions
+// // Prompt 5 yes or no questions
 
-var meghanTeacherGuess = prompt('Was Meghan a teacher?');
-var meghanTeacher = meghanTeacherGuess.toLowerCase();
-// console.log('meghanTeacher', meghanTeacher);
+// var meghanTeacherGuess = prompt('Was Meghan a teacher?');
+// var meghanTeacher = meghanTeacherGuess.toLowerCase();
+// // console.log('meghanTeacher', meghanTeacher);
 
-if (meghanTeacher === 'yes') {
-  alert('Correct, Meghan taught at Bright Horizons.');
-} else {
-  alert('Incorrect, Meghan was a teacher at Bright Horizons.');
-}
+// if (meghanTeacher === 'yes') {
+//   alert('Correct, Meghan taught at Bright Horizons.');
+// } else {
+//   alert('Incorrect, Meghan was a teacher at Bright Horizons.');
+// }
 
-var stateLiveAnswer = prompt('Does Meghan live in Ohio?');
-var stateLive = stateLiveAnswer.toLowerCase();
-// console.log('stateLive', stateLive);
+// var stateLiveAnswer = prompt('Does Meghan live in Ohio?');
+// var stateLive = stateLiveAnswer.toLowerCase();
+// // console.log('stateLive', stateLive);
 
-if (stateLive === 'yes') {
-  alert('Correct, ');
-} else {
-  alert('Incorrect');
-}
+// if (stateLive === 'yes') {
+//   alert('Correct, ');
+// } else {
+//   alert('Incorrect');
+// }
 
-var playCelloAnswer = prompt('Does Meghan play the cello?');
-var playCello = playCelloAnswer.toLowerCase();
-// console.log('playCello', playCello);
+// var playCelloAnswer = prompt('Does Meghan play the cello?');
+// var playCello = playCelloAnswer.toLowerCase();
+// // console.log('playCello', playCello);
 
-if (playCello === 'yes') {
-  alert('Correct');
-} else {
-  alert('Incorrect');
-}
-var hasDaughterAnswer = prompt('Does Meghan have a daughter?');
-var hasDaughter = hasDaughterAnswer.toLowerCase();
-// console.log('hasDaughter', hasDaughter);
+// if (playCello === 'yes') {
+//   alert('Correct');
+// } else {
+//   alert('Incorrect');
+// }
+// var hasDaughterAnswer = prompt('Does Meghan have a daughter?');
+// var hasDaughter = hasDaughterAnswer.toLowerCase();
+// // console.log('hasDaughter', hasDaughter);
 
-if (hasDaughter === 'yes') {
-  alert('Correct');
-} else {
-  alert('Incorrect');
-}
+// if (hasDaughter === 'yes') {
+//   alert('Correct');
+// } else {
+//   alert('Incorrect');
+// }
 
-var nextJobAnswer = prompt('Is Meghan a software developer?');
-var nextJob = nextJobAnswer.toLowerCase();
-// console.log('nextJob', nextJob);
+// var nextJobAnswer = prompt('Is Meghan a software developer?');
+// var nextJob = nextJobAnswer.toLowerCase();
+// // console.log('nextJob', nextJob);
 
-if (nextJob === 'yes') {
-  alert('Correct');
-} else {
-  alert('Incorrect');
-}
+// if (nextJob === 'yes') {
+//   alert('Correct');
+// } else {
+//   alert('Incorrect');
+// }
 
-alert('It was nice having you ' + firstNameAnswer + '. Hope you enjoyed learning a little about me!');
+// alert('It was nice having you ' + firstNameAnswer + '. Hope you enjoyed learning a little about me!');
